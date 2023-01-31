7612576f358a19599af307d65ef527900da07b4b
mihadereani <110335459+mihadereani@users.noreply.github.com>
2023-01-31 08:17:38 +0100

    add TextInput

--- App.js
+++ App.js
@@ -1,34 +1,34 @@
 import React, { Component } from 'react';
-import { StyleSheet, View } from 'react-native';
-export default class HelloWorld extends Component {
+import { StyleSheet, View, Text, TextInput, Alert, Button } from 'react-native';
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { text: '' };
+  }
+
+  // alert the user input
+  alertMyText(input = []) {
+    Alert.alert(input.text);
+  }
+
   render() {
     return (
-      <View style={{ flex: 1, flexDirection: 'column' }}>
-        <View style={{ width: 50, height: 50, backgroundColor: 'blue' }}></View>
-        <View style={{ flex: 1, backgroundColor: 'red' }}></View>
-        <View
-          style={{ width: 100, height: 200, backgroundColor: 'green' }}
-        ></View>
+      <View style={{ flex: 1, justifyContent: 'center' }}>
+        <TextInput
+          style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+          onChangeText={(text) => this.setState({ text })}
+          value={this.state.text}
+          placeholder='Type here ...'
+        />
+        <Text>You wrote: {this.state.text}</Text>
+        <Button
+          onPress={() => {
+            this.alertMyText({ text: this.state.text });
+          }}
+          title='Press Me'
+        />
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'column',
-  },
-  box1: {
-    flex: 10,
-    backgroundColor: 'blue',
-  },
-  box2: {
-    flex: 120,
-    backgroundColor: 'red',
-  },
-  box3: {
-    flex: 50,
-    backgroundColor: 'green',
-  },
-});
