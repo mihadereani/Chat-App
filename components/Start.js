0ab81eedb93c87894e09423ac5eb2266af73d488
mihadereani <110335459+mihadereani@users.noreply.github.com>
2023-01-31 21:51:10 +0100

    add start screen

--- components/Start.js
+++ components/Start.js
@@ -1,29 +1,29 @@
 import React from 'react';
-import { View, Text, Button, TextInput } from 'react-native';
+import { View, Text, Button, TextInput, StyleSheet } from 'react-native';
 
-export default class Screen1 extends React.Component {
+export default class Start extends React.Component {
   render() {
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Hello Screen1!</Text>
-        <TextInput
-          style={styles.input}
-          onChangeText={onChangeNumber}
-          value={number}
-          placeholder='useless placeholder'
-          keyboardType='numeric'
-        />
-        <Button
-          onPress={onPressLearnMore}
-          title='Learn More'
-          color='#841584'
-          accessibilityLabel='Learn more about this purple button'
-        />
-        <Button
-          title='Go to Screen 2'
-          onPress={() => this.props.navigation.navigate('Screen2')}
-        />
+        <Text>App Title</Text>
+        <View
+          style={{ flex: 0.44, justifyContent: 'center', alignItems: 'center' }}
+        >
+          <TextInput
+            style={styles.input}
+            placeholder='useless placeholder'
+            keyboardType='numeric'
+          />
+          <Button
+            title='Start Chating'
+            color='#841584'
+            accessibilityLabel='Start Chating'
+            onPress={() => this.props.navigation.navigate('Chat')}
+          />
+        </View>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({});
