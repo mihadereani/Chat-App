c142aab297b39d4614cdfc093d6988080e0e1f4c
mihadereani <110335459+mihadereani@users.noreply.github.com>
2023-02-02 19:21:33 +0100

    fix bug

--- components/Chat.js
+++ components/Chat.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
-export default class Screen2 extends React.Component {
+export default class Chat extends React.Component {
   render() {
     let name = this.props.route.params.name;
-    this.props.navigation.setOptions({ title: name });
-
     let color = this.props.route.params.color;
+
+    this.props.navigation.setOptions({ title: name });
 
     return (
       <View
@@ -17,7 +17,7 @@ export default class Screen2 extends React.Component {
           backgroundColor: color,
         }}
       >
-        <Text>Hello Screen2!</Text>
+        <Text>Lets Chat</Text>
       </View>
     );
   }
