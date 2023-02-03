6e338a4a5c75626291f2b987761febd0e5bd6ee5
mihadereani <110335459+mihadereani@users.noreply.github.com>
2023-02-03 21:16:56 +0100

    add name

--- components/Chat.js
+++ components/Chat.js
@@ -34,6 +34,9 @@ export default class Chat extends React.Component {
   }
 
   render() {
+    let name = this.props.route.params.name;
+    this.props.navigation.setOptions({ title: name });
+
     return (
       <GiftedChat
         messages={this.state.messages}
@@ -44,4 +47,23 @@ export default class Chat extends React.Component {
       />
     );
   }
+
+  /* render() {
+    
+
+    let color = this.props.route.params.color;
+
+    return (
+      <View
+        style={{
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center',
+          backgroundColor: color,
+        }}
+      >
+        <Text>Lets Chat</Text>
+      </View>
+    );
+  } */
 }
