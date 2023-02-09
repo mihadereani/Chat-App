a136d035c9d63b7df15e970076f1c8fa04242ae6
mihadereani <110335459+mihadereani@users.noreply.github.com>
2023-02-09 23:20:45 +0100

    clean code

--- components/Chat.js
+++ components/Chat.js
@@ -49,27 +49,6 @@ export default class Chat extends React.Component {
         .orderBy('createdAt', 'desc')
         .onSnapshot(this.onCollectionUpdate);
     });
-
-    /* this.setState({
-      messages: [
-        {
-          _id: 1,
-          text: 'Hello developer',
-          createdAt: new Date(),
-          user: {
-            _id: 2,
-            name: 'React Native',
-            avatar: 'https://placeimg.com/140/140/any',
-          },
-        },
-        {
-          _id: 2,
-          text: name + ' entered the chat.',
-          createdAt: new Date(),
-          system: true,
-        },
-      ],
-    }); */
   }
 
   onCollectionUpdate = (querySnapshot) => {
